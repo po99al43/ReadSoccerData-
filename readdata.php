c12b501c5fddfbc84c601b1e934313890a2800ca
Ryan <po99al43@gmail.com>
2016-08-18 07:41:45 +0000

    寫入memcached背景執行完成

--- readdata.php
+++ readdata.php
@@ -2,8 +2,22 @@
 
 require_once("connect.php");
 
-$conn = new Connect;
-$alldat = $conn->getgame();
-$mc = new Memcached();
-$mc->addServer("localhost", 11211);
-$mc->set("data", json_encode($alldat));
+$type = true;
+ignore_user_abort();//關掉瀏覽器，PHP腳本也可以繼續執行.
+
+set_time_limit(0);// 通過set_time_limit(0)可以讓程式無限制的執行下去
+
+$interval=120;//設定運行1分鐘
+
+do {
+
+    $conn = new Connect;
+    $alldat = $conn->getgame();
+
+    $mc = new Memcached();
+    $mc->addServer("localhost", 11211);
+
+    $mc->set("data", json_encode($alldat));
+    sleep($interval);// 等待1分鐘
+
+} while (true);
