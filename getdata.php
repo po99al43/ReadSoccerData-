bcc8b15aa50d529f4704d55a47a393cf5fa3231c
Ryan <po99al43@gmail.com>
2016-08-18 07:41:05 +0000

    背景執行完成

--- getdata.php
+++ getdata.php
@@ -3,33 +3,42 @@ header("content-type: text/html; charset=utf8");
 
 require_once("connect.php");
 
-// ignore_user_abort();//關掉瀏覽器，PHP腳本也可以繼續執行.
+$type = true;
+ignore_user_abort($type);//關掉瀏覽器，PHP腳本也可以繼續執行.
 
-// set_time_limit(0);// 通過set_time_limit(0)可以讓程式無限制的執行下去
+set_time_limit(0);// 通過set_time_limit(0)可以讓程式無限制的執行下去
 
-$interval=60;// 每隔半小時運行
+$interval=60;//設定運行1分鐘
 
-// do {
-    $agent = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
+do {
 
+    //設定url
+    $url = "http://www.228365365.com/app/member/FT_browse/body_var.php?uid=test00&rtype=r&langx=zh-cn&mtype=3&delay=&league_id=";
     $ch = curl_init();
 
-    //設定 / 調
-    //curl_setopt($ch, CURLOPT_URL, "http://www.228365365.com/app/member/FT_browse/body_var.php?uid=test00&rtype=r&langx=zh-cn&mtype=3&delay=&league_id=");
-    curl_setopt($ch, CURLOPT_URL, "http://www.228365365.com/app/member/FT_future/body_var.php?uid=test00&rtype=r&langx=zh-cn&mtype=3&page_no=0&league_id=&hot_game=");
+    //先取得所需要的cookie
+    curl_setopt($ch, CURLOPT_URL,"http://www.228365365.com/sports.php");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+    curl_exec($ch);
+
+    //傳回cookie取得所需要的資料
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
     curl_setopt($ch, CURLOPT_HEADER, 0);
 
-    // 3. 執行，取回 response 結果
+    //執行，取回 response 結果存入text.txt中
     $pageContent = curl_exec($ch);
     $fo = fopen ('text.txt' , 'w');
     fwrite ($fo, $pageContent );
 
-    // 4. 關閉與釋放資源
+    //關閉與釋放資源
     curl_close($ch);
     fclose($fo);
 
+    //將text.txt讀出並做解析
     $fo = fopen ('text.txt', 'r' );
     $result;
     while (! feof($fo)) {
@@ -43,21 +52,20 @@ $interval=60;// 每隔半小時運行
         }
     }
 
+    //對解析內容做php語法判斷
     eval($result);
+
     fclose($fo);
-    echo "<hr>";
+
+    //判斷是否有解析後的內容
+    if ($GameFT) {
+    //將內容寫進資料庫
     $conn = new Connect();
     $conn->savegame($GameFT);
-    foreach($GameFT as $k => $v)
-    {
-        echo $k."<br>" ;
-        foreach($GameFT[$k] as $j => $g){
-        echo "\t[".$j."]".$GameFT[$k][$j]."<br>";}
     }
 
-    sleep($interval);// 等待5分鐘
-// } while (true);
-
+    sleep($interval);// 等待1分鐘
+} while (true);
 
 
 
